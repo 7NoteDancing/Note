e122e8f6b439fb40c305217f371409e646eb5a14
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-08 15:09:44 +0000

    Update Q_learning.py

--- Note/RL/TD/Q_learning.py
+++ Note/RL/TD/Q_learning.py
@@ -134,6 +134,7 @@ class Q_learning:
             output_file=open(path+'.dat','wb')
         else:
             output_file=open(path+'-{0}.dat'.format(i+1),'wb')
+        pickle.dump(self.episode,output_file)
         pickle.dump(self.action_len,output_file)
         pickle.dump(self.action,output_file)
         pickle.dump(self.action_prob,output_file)
@@ -153,6 +154,7 @@ class Q_learning:
     
     def restore(self,path):
         input_file=open(path,'rb')
+        self.episode=pickle.load(input_file)
         self.action_len=pickle.load(input_file)
         if self.action_len==len(self.action_name):
             self.action=pickle.load(input_file)
