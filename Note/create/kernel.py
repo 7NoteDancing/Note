fab395a47bbb5ebd2270696a13abc0fc917eab9c
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-12 04:26:28 +0000

    Update kernel.py

--- Note/create/kernel.py
+++ Note/create/kernel.py
@@ -173,6 +173,8 @@ class kernel:
                                 else:
                                     gradient=tape.gradient(batch_loss,self.parameter)
                                     self.optimizern(gradient,self.parameter)
+                    if i==(epoch-1):
+                        self.output=self.nn.forward_propagation(data_batch,self.dropout)
                     total_loss+=batch_loss
                     if self.acc_flag1==1:
                         with tf.name_scope('accuracy'):
@@ -219,6 +221,8 @@ class kernel:
                                 else:
                                     gradient=tape.gradient(batch_loss,self.parameter)
                                     self.optimizern(gradient,self.parameter)
+                    if i==(epoch-1):
+                        self.output=self.nn.forward_propagation(data_batch,self.dropout)
                     total_loss+=batch_loss
                     if self.acc_flag1==1:
                         with tf.name_scope('accuracy'):
@@ -287,6 +291,8 @@ class kernel:
                            else:
                                gradient=tape.gradient(batch_loss,self.parameter)
                                self.optimizern(gradient,self.parameter)
+                if i==(epoch-1):
+                    self.output=self.nn.forward_propagation(data_batch,self.dropout)
                 self.train_loss_list.append(loss.astype(np.float32))
                 self.train_loss=loss
                 self.train_loss=self.train_loss.astype(np.float32)
