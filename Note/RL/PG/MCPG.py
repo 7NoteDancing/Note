03f98fac52f6f7352e7a182bccd804c1fc24f632
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-27 04:00:23 +0000

    Update MCPG.py

--- Note/RL/PG/MCPG.py
+++ Note/RL/PG/MCPG.py
@@ -154,8 +154,7 @@ class MCPG:
         input_file=open(path,'rb')
         self.episode=pickle.load(input_file)
         self.action_len=pickle.load(input_file)
-        if self.action_len==len(self.action_name):
-            self.action=pickle.load(input_file)
+        self.action=pickle.load(input_file)
         self.reward_list=pickle.load(input_file)
         self.epsilon=pickle.load(input_file)
         self.discount=pickle.load(input_file)
