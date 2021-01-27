8e06cb3d19567a2fd92aadd9350d1338174c1d5f
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-27 03:59:54 +0000

    Update Sarsa.py

--- Note/RL/TD/Sarsa.py
+++ Note/RL/TD/Sarsa.py
@@ -159,9 +159,8 @@ class Sarsa:
         input_file=open(path,'rb')
         self.episode=pickle.load(input_file)
         self.action_len=pickle.load(input_file)
-        if self.action_len==len(self.action_name):
-            self.action=pickle.load(input_file)
-            self.action_onerob=pickle.load(input_file)
+        self.action=pickle.load(input_file)
+        self.action_onerob=pickle.load(input_file)
         self.epsilon=pickle.load(input_file)
         self.alpha=pickle.load(input_file)
         self.discount=pickle.load(input_file)
