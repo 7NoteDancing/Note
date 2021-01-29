16af0ffefccea33cbf0d8f5c7964efd4aa1705ac
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-29 14:17:19 +0000

    Update MCPG.py

--- Note/RL/PG/MCPG.py
+++ Note/RL/PG/MCPG.py
@@ -54,6 +54,8 @@ class MCPG:
                 output=self.policy_net(self.state[s])
                 a=np.random.choice(action,output)
                 next_s,r,end=self.exploration_space[self.state_name[s]][self.action_name[a]]
+                G+=r
+                loss+=self.loss(output,G)
                 if end:
                     self.reward_list.append(G)
                     if self.save_episode==True:
@@ -61,8 +63,6 @@ class MCPG:
                     break
                 if self.save_episode==True:
                     episode.append([self.state_name[s],self.action_name[a],r])
-                G+=r
-                loss+=self.loss(output,G)
                 s=next_s
         else:
             output=self.policy_net(self.state[s])
@@ -70,6 +70,8 @@ class MCPG:
                 output=self.policy_net(self.state[s])
                 a=np.random.choice(action,output)
                 next_s,r,end=self.exploration_space[self.state_name[s]][self.action_name[a]]
+                G+=r
+                loss+=self.loss(output,G)
                 if end:
                     self.reward_list.append(G)
                     if self.save_episode==True:
@@ -77,8 +79,6 @@ class MCPG:
                     break
                 if self.save_episode==True:
                     episode.append([self.state_name[s],self.action_name[a],r])
-                G+=r
-                loss+=self.loss(output,G)
                 s=next_s
             self.reward_list.append(G)
         if self.save_episode==True:
